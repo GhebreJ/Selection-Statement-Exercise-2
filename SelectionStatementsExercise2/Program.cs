567de32a2de1f9224627a5276f39b5e9537e7249
Jonah <jghebremichael95@gmail.com>
2023-01-02 18:41:25 -0500

    done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject");
+
+            var subject = Console.ReadLine();
+
+            switch(subject.ToLower()) 
+            {
+                case "math": 
+                    Console.WriteLine("Nerd");
+                    break;
+                case "english":
+                    Console.WriteLine("Theatre Kid");
+                    break;
+                case "pe":
+                    Console.WriteLine("Jock");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("Mr.White");
+                    break;
+                default:
+                    Console.WriteLine("No idea what this subject is");
+                    break;
+                 
+            }
         }
     }
 }
